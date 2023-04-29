0999438afc810cfe3afa57a774f3a42633862b6e
Hina Khan <47082795+Hana-Khan@users.noreply.github.com>
2023-04-29 23:11:16 +0500

    routing

--- main.go
+++ main.go
@@ -1,4 +1,30 @@
 package main
-
+import (
+	"crud-rest-api-in-golang/controllers"
+	"crud-rest-api-in-golang/database"
+)
 func main() {
+	// Load Configurations from config.json using Viper
+	LoadAppConfig()
+
+	// Initialize Database
+	database.Connect(AppConfig.ConnectionString)
+	database.Migrate()
+	
+	// Initialize the router || Creates a new instance of the MUX Router.
+	router := mux.NewRouter().StrictSlash(true)
+
+	// Register the Product Route Handlers into the MUX router.
+	RegisterProductRoutes(router)
+
+	// Start the server
+	log.Println(fmt.Sprintf("Starting Server on port %s", AppConfig.Port))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", AppConfig.Port), router))
+}
+func RegisterProductRoutes(router *mux.Router) {
+	router.HandleFunc("/api/products", controllers.GetProducts).Methods("GET")
+	router.HandleFunc("/api/products/{id}", controllers.GetProductById).Methods("GET")
+	router.HandleFunc("/api/products", controllers.CreateProduct).Methods("POST")
+	router.HandleFunc("/api/products/{id}", controllers.UpdateProduct).Methods("PUT")
+	router.HandleFunc("/api/products/{id}", controllers.DeleteProduct).Methods("DELETE")
 }
